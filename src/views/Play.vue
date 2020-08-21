1540465fd9600a3b77f07577f4ef4dd7461fce9d
Shivam Kumar <shivambaku@gmail.com>
2020-08-21 06:44:56 -0700

    fixed mor

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="app">
-    <div class='header'>
-      <h1>Connect 9</h1>
+    <Game/>
+    <div class='rules'>
+      <h4>How to Play</h4>
       <p>
-        Click on one of the empty spots to place the selected number.
+        Select one of the three available numbers and place it in an empty spot.
         <br/>
-        You can select one of the three available numbers.
-        <br/>
-        The selected one will be placed and a new random number (1-5) will replace it.
+        Connect three or more of the same adjacent numbers to get a higher number.
       </p>
     </div>
-    <Game/>
+    <hr/>
     <div class='footer'>
       Connect 9 is made by Shivam Kumar and Cody Harris.
     </div>

16e3703a52f80a41a6a8af5e157bef6d9cc2e4bc
Shivam Kumar <hei@Shivams-MacBook-Pro.local>
2020-08-21 15:30:24 -0700

    Update App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <router-view/>
     <router-link to="/">Play</router-link>
+    <span style='display:inline-block; width: 10px;'/>
     <router-link to="multiplayer">Multiplayer</router-link>
   </div>
 </template>

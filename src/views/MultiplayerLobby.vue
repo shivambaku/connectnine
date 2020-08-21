a909c1488001ef397fe44850344786b8e8a9f279
codyblharris <codyblharris@gmail.com>
2020-08-21 02:13:52 -0700

    Server Client working still needs socket passing

---
+++ src/views/MultiplayerLobby.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1> Welcome To The Multiplayer Lobby! </h1>
+        {{id}}
+        <p> {{Players}} </p>
+        <button v-on:click="submit"> Start </button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'MultiplayerLobby',
+    props:[
+        'id',
+    ],
+    data() {
+        return {
+            Players: []
+        }
+    },
+    created() {
+        this.$socket.on("users", data => {
+            this.Players = data;
+        })
+    }
+}
+</script>

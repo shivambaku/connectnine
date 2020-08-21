9af7eb26152e52df7690a6fb209e37388d64f04b
Shivam Kumar <shivambaku@gmail.com>
2020-08-21 06:40:43 -0700

    fixed the branch

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
-      <div id="app">
-        <router-view/>
-        <router-link to="/"> Play </router-link>
-        <router-link to="Multiplayer"> Multiplayer </router-link>
-
-    </div>
+  <div id="app">
+    <router-view/>
+    <router-link to="/"> Play </router-link>
+    <router-link to="Multiplayer"> Multiplayer </router-link>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'app'
-}
+  name: 'app',
+};
 </script>
